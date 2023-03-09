3111dbccdc4024a1008af360e947e6725f968d44
dgbdevs <dgb.develop@gmail.com>
2023-03-09 02:30:18 -0300

    Grilla v1.0 Server

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
  const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors');
+
+
+app.use(cors({
+    origin: '*'
+}));
 
 //settings 
 app.set('port', process.env.PORT || 3000)
